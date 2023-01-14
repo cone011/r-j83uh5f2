e4bd1c7894cd6a18694b1167b4d29650c8e6302e
Juan <jjcaimen@gmail.com>
2023-01-14 13:05:15 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component, useState } from "react";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="container">
-        <textarea rows="3"></textarea>
-        <div className="counter">0</div>
-      </div>
-    );
-  }
+function App() {
+  const [long, SetLong] = useState(0);
+  const valueChangedHandler = (event) => {
+    let lengthValue = event.target.value.length;
+    SetLong(lengthValue);
+  };
+
+  return (
+    <div className="container">
+      <textarea rows="3" onChange={valueChangedHandler}></textarea>
+      <div className="counter">{long}</div>
+    </div>
+  );
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="container">
+//         <textarea rows="3"></textarea>
+//         <div className="counter">0</div>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
